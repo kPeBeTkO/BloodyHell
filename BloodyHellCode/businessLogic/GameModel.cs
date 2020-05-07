c25547de5b25ba1eaa598905f20e64f30a4cabf5
kPeBeTkO <57574137+kPeBeTkO@users.noreply.github.com>
2020-05-08 03:44:35 +0500

    Написал модель игры, дополнил Level

--- BloodyHellCode/businessLogic/GameModel.cs
+++ BloodyHellCode/businessLogic/GameModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,64 @@ using System.Threading.Tasks;
 
 namespace BloodyHell
 {
+    public enum GameState
+    {
+        Menu,
+        InGame,
+        Pause
+    }
+
     class GameModel
     {
-        private List<Level> levels;
+        private Stopwatch watch = new Stopwatch();
+        private long lastFrame;
+        private long frameTime;
+        public readonly List<Level> Levels;
+        public GameState curentState { get; private set; } = GameState.Menu;
+        private int curentLevelID;
+        public Level CurentLevel { get { return Levels[curentLevelID]; } }
+        public GameModel(List<string> levelNames)
+        {
+            Levels = new List<Level>();
+            foreach (var name in levelNames)
+                Levels.Add(new Level(name));
+        }
+
+        public void Start()
+        {
+            if (curentState == GameState.Menu)
+            {
+                curentLevelID = 0;
+                curentState = GameState.InGame;
+                watch.Start();
+            }
+            else
+                throw new Exception("Попытка начать игру вне меню");
+        }
+
+        public void Pause()
+        {
+            switch (curentState)
+            {
+                case GameState.InGame:
+                    curentState = GameState.Pause;
+                    watch.Stop();
+                    break;
+                case GameState.Pause:
+                    curentState = GameState.InGame;
+                    watch.Start();
+                    break;
+                default:
+                    throw new Exception("попытка зайти в паузу вне игры");
+            }
+        }
+
+        public void Update()
+        {
+            var curent = watch.ElapsedMilliseconds;
+            frameTime = curent - lastFrame;
+            lastFrame = curent;
+            CurentLevel.Update(frameTime);
+        }
     }
 }
--- BloodyHellCode/businessLogic/Level.cs
+++ BloodyHellCode/businessLogic/Level.cs
@@ -3,19 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using BloodyHell.Entities;
+using RayCasting;
 
 namespace BloodyHell
 {
     class Level
     {
-        public Map Map;
+        public readonly string LevelName;
+        public Map Map { get; private set; }
         public List<IEntity> Entities;
-        public Player Player;
+        public Player Player { get; private set; }
+        public Vector Exit { get; private set; }
+        public Level(string levelName)
+        {
+            LevelName = levelName;
+            LoadFromFile();
+        }
         public Level(Map map, Player player)
         {
             Map = map;
             Player = player;
+        }
+
+        public void LoadFromFile()
+        {
+            var file = new StreamReader("LevelData/" + LevelName + ".txt");
+            var n = int.Parse(file.ReadLine());
+            for (var i = 0; i < n; i++)
+            {
+                var line = file.ReadLine().Split();
+                switch(line[0])
+                {
+                    case "Player":
+                        Player = new Player(new Vector(int.Parse(line[1]), int.Parse(line[2])));
+                        break;
+                    case "Exit":
+                        Exit = new Vector(int.Parse(line[1]), int.Parse(line[2]));
+                        break;
+                    case "Monster":
+                        //надо дописать
+                        break;
+                }
+            }
         }
 
         public void Update(long timeElapsed)
